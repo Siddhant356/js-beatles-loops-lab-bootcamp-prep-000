7be156da5eed9c06cb5c82f87c76cfd1ee02ad49
Siddhant Shukla <siddhantshukla356@gmail.com>
2019-04-30 13:02:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // add solution here
+function theBeatlesPlay(musicians,instrument){
+ var array=[]
+ for(var i=0;i<musicians.length;i++){
+   array[i]=`${musicians[i]} plays ${instrument[i]}` 
+ }
+ return array
+}
+function johnLennonFacts(array1){
+  var i=0
+  while(array1.length-1!==i){
+    array1[i]=array1[i]+'!!!'
+    i++
+  }
+  return array1
+}
